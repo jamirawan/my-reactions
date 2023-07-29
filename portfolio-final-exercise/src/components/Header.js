e9a0a89bdc81b617508f1ad10a1f3204996c0ab9
Irawan <irawan@live.com.au>
2023-07-29 16:16:11 +0800

    updated header, footer and landing section

--- portfolio-final-exercise/src/components/Header.js
+++ portfolio-final-exercise/src/components/Header.js
@@ -2,7 +2,12 @@ import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
+  faArtstation,
+  faDrupal,
+  faFacebook,
+  faFreeCodeCamp,
   faGithub,
+  faInstagram,
   faLinkedin,
   faMedium,
   faStackOverflow,
@@ -11,28 +16,37 @@ import { Box, HStack } from "@chakra-ui/react";
 
 const socials = [
   {
+    icon: faFacebook,
+    url: "https://www.facebook.com/irawan.digital",
+  },
+  {
+    icon: faArtstation,
+    url: "https://www.artstation.com/iirawan",
+  },
+  {
     icon: faEnvelope,
-    url: "mailto: hello@example.com",
+    url: "mailto:me@irawan",
   },
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: "https://github.com/jamirawan",
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
+    url: "https://www.linkedin.com/irawanirawan",
   },
   {
-    icon: faMedium,
-    url: "https://medium.com",
+    icon: faDrupal,
+    url: "https://drupal.com/irawan",
   },
   {
-    icon: faStackOverflow,
-    url: "https://stackoverflow.com",
+    icon: faFreeCodeCamp,
+    url: "hhttps://www.freecodecamp.org/iirawan",
   },
 ];
 
 const Header = () => {
+  // Handle clicks on buttons within the header
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -44,6 +58,37 @@ const Header = () => {
     }
   };
 
+  // Handle header show/hide animation depending on the scroll direction
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    // Handle scroll events
+    const handleScroll = () => {
+      const currScrollPos = window.scrollY;
+      const currHeaderElement = headerRef.current;
+
+      if (!currHeaderElement)
+        return;
+    
+      if (prevScrollPos > currScrollPos)
+        currHeaderElement.style.transform = "translateY(0)";
+      else
+        currHeaderElement.style.transform = "translateY(-200px)";
+      
+      prevScrollPos = currScrollPos;
+    };
+
+    // Set up listeners for the scroll event
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove listeners for the scroll event
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Box
       position="fixed"
@@ -54,9 +99,10 @@ const Header = () => {
       transitionProperty="transform"
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
+      backgroundColor="#3fb99a"
+      ref={headerRef}
     >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
+      <Box color="#34495f" maxWidth="1280px" margin="0 auto">
         <HStack
           px={16}
           py={4}
@@ -65,9 +111,20 @@ const Header = () => {
         >
           <nav>
             {/* Add social media links based on the `socials` data */}
+            <HStack spacing={10}>
+              {socials.map(({icon, url}) => (
+                <a key={url} href={url} icon={icon} target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon key={url} icon={icon} size="2x"/>
+                </a>
+              ))}
+            </HStack>
+            {/* Add social media links based on the `socials` data */}
           </nav>
           <nav>
             <HStack spacing={8}>
+              {/* Add links to Projects and Contact me section */}
+              <a href="#projects" onClick={handleClick("projects")}>Projects</a>
+              <a href="#contact" onClick={handleClick("contactme")}>Contact Me</a>
               {/* Add links to Projects and Contact me section */}
             </HStack>
           </nav>
@@ -76,4 +133,5 @@ const Header = () => {
     </Box>
   );
 };
+
 export default Header;
--- portfolio-final-exercise/src/components/LandingSection.js
+++ portfolio-final-exercise/src/components/LandingSection.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+// const greeting = "Irawan here, apakabar?";
+const bio1 = "Irawan's web portfolio";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -13,9 +12,19 @@ const LandingSection = () => (
     justifyContent="center"
     alignItems="center"
     isDarkBackground
-    backgroundColor="#2A4365"
+    backgroundColor="#34495f"
   >
-
+    {/* The UI for the landing section */}
+    <VStack spacing={16}>
+      <VStack spacing={4}>
+        <Avatar src="https://irawan.io/images/irawan.png" size="2xl" name="Irawan" />
+        <Heading as="p" size="lg" noOfLines={1}>{greeting}</Heading>
+      </VStack>
+      <VStack spacing={6}>
+        <Heading as="h1" size="2xl" noOfLines={1}>{bio1}</Heading>
+      </VStack>
+    </VStack>
+    {/* The UI for the landing section */}
   </FullScreenSection>
 );
 
